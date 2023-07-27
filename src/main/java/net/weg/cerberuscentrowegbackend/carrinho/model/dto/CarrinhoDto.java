7e47d6001120c400091b21a1d978d463c663e407
KAUÊ RAFAEL VOLLES FREITAS TEIXEIRA <kaue_teixeira@estudante.sesisenai.org.br>
2023-07-27 16:48:03 -0300

    Doing cart stuff

--- src/main/java/net/weg/cerberuscentrowegbackend/carrinho/model/dto/CarrinhoDto.java
+++ src/main/java/net/weg/cerberuscentrowegbackend/carrinho/model/dto/CarrinhoDto.java
@@ -3,6 +3,7 @@ package net.weg.cerberuscentrowegbackend.carrinho.model.dto;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import net.weg.cerberuscentrowegbackend.pessoa.model.entity.Pessoa;
 import net.weg.cerberuscentrowegbackend.produto.model.entity.Produto;
 import net.weg.cerberuscentrowegbackend.pessoa.model.entity.PessoaFisica;
 
@@ -15,6 +16,6 @@ public class CarrinhoDto {
     private List<Produto> produtos;
 
     @NotNull
-    private PessoaFisica pessoaFisica;
+    private Pessoa pessoa;
 
 }
