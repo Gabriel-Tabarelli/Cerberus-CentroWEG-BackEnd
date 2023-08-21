936dd56083e98c3de92b15231a9c04ccff6ef16d
KaueTeixxeira <kaueteixerak9@hotmail.com>
2023-08-21 16:53:55 -0300

    Ajustes de ordem de pedido

--- src/main/java/net/weg/cerberuscentrowegbackend/pedido/model/projection/PedidoListaProjection.java
+++ src/main/java/net/weg/cerberuscentrowegbackend/pedido/model/projection/PedidoListaProjection.java
@@ -8,6 +8,6 @@ public interface PedidoListaProjection {
 
     String getStatus();
 
-    LocalDate getDataPedido();
+    String getDataPedido();
 
 }
--- src/main/java/net/weg/cerberuscentrowegbackend/pedido/service/PedidoService.java
+++ src/main/java/net/weg/cerberuscentrowegbackend/pedido/service/PedidoService.java
@@ -33,9 +33,14 @@ public class PedidoService {
     }
 
     public Page<PedidoListaProjection> findAll(Long pessoaId, Integer page, String ordem) {
-        Sort sort = Sort.by(Sort.Direction.DESC, "dataPedido");
+        Sort sort = null;
+        System.out.println(ordem);
         if (ordem.equals("asc")) {
-            sort = Sort.by(Sort.Direction.ASC, "dataPedido");
+            System.out.println("ASC");
+            sort = Sort.by("id").ascending();
+        } else {
+            System.out.println("DESC");
+            sort = Sort.by("id").descending();
         }
         return repository.findByPessoa_Id(pessoaId, PageRequest.of(page, 5, sort));
     }
